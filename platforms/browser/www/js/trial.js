a23b84a6ff4f0c0a251c8f1125e0a899a4274d14
Edward H.M <edwardhm865@gmail.com>
2019-11-05 16:07:53 +0700

    websql

--- platforms/browser/www/js/trial.js
+++ platforms/browser/www/js/trial.js
@@ -3,23 +3,35 @@ $(document).ready(function(){
     $("#trial").click(function(){
 
         //mengirimkan data username dan password dan me-return data user beserta jwt token
-        $.ajax({ 
-            url: 'http://127.0.0.1:8888/signin',
-            dataType: 'json',
-            contentType : "application/json",
-            method : 'POST',
-            cache: false,
-            data: JSON.stringify( { "name": $('#nama').val(),
-             "kontak": $('#phone').val(),"status": $('anggota').val(), "password": $('#password').val() } ),
-            processData: false,
+        // $.ajax({ 
+        //     url: 'http://127.0.0.1:8888/signin',
+        //     dataType: 'json',
+        //     contentType : "application/json",
+        //     method : 'POST',
+        //     cache: false,
+        //     data: JSON.stringify( { "name": $('#nama').val(),
+        //      "kontak": $('#phone').val(),"status": $('anggota').val(), "password": $('#password').val() } ),
+        //     processData: false,
 
-            success : function(result){            
-                if(result["status"] == "success"){
-                 alert("sukses bro")
-                } else{
-                    alert("gagal bro");
+        //     success : function(result){            
+        //         if(result["status"] == "success"){
+        //          alert("sukses bro")
+        //         } else{
+        //             alert("gagal bro");
+        //         }
+        //     }
+        // });
+
+            $.ajax({
+                type: 'GET',
+                url: "http://192.168.1.6/api_presensi_basil/trial.php",
+                success:function(result){
+                 if(result["status"]  == "success"){
+                    alert("berhasil koneksi dengan api")
+                 } else{
+                     alert("coba lagi");
+                 }
                 }
-            }
-        });
+            });
     });
 });  
